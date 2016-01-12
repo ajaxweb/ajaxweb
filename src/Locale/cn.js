c66587eb144bcf8ae4b7b98c77facbafc3869d3d
Dennis M. Heine <ajaxweb@heine.ml>
2016-01-12 09:48:58 +0100

    -Added license headers -Updated documentation

--- src/Locale/cn.js
+++ src/Locale/cn.js
@@ -1,4 +1,20 @@
-﻿(linb.Locale.cn||(linb.Locale.cn={})).VisualJS={
+/*
+    This file is part of Ajax Web Developer.
+
+    Ajax Web Developer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ajax Web Developer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ajax Web Developer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+(linb.Locale.cn||(linb.Locale.cn={})).VisualJS={
     message:"系统消息!",
     noMessage:"尚无任何消息!",
     soon:'即将实现',
